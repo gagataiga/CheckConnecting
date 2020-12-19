c1ac1de5ed6f6f512132e886476eb002158e655f
gagataiga <gaitatg97@gmail.com>
2020-12-20 06:30:57 +0900

    ping疎通用バッチの作成

--- main.cmd
+++ main.cmd
@@ -1,4 +1,20 @@
 @echo off
-:: serverlist分のping疎通を実行
+:: ping to the IP address in the serverList.lst
+
+:: your current dir
+SET CURRENTDIR=%~dp0
+:: Target Server List
+SET LIST=%CURRENTDIR%serverList.lst
+
+for /f "tokens=1" %%i in (%LIST%) do (
+echo
+echo %%i
+start %CURRENTDIR%ping_check.cmd %%i
+echo
+echo -------------------------------
+
+)
+
+pause
 
 
